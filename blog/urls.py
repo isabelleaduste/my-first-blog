8a571f492cfea523ddad637f7d3b519b2de6df5e
isabelle <isabelleaduste@gmail.com>
2017-08-02 09:16:07 -0700

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     #url(r'^environmentalhome.html/$', views.environmental, name='environmental'),
     #url(r'^socialhome.html/$', views.social, name='social'),
     #url(r'^politicalhome.html/$', views.political, name='political'),
-
-
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post, Comment
+
 
 
 def post_list(request):
@@ -40,6 +43,20 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
 
 #def environmental(request):
     #return render(request, 'blog/environmentalhome.html', {})
